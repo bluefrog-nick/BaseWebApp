b0aa2d837620b6a1de12818ab8c77494036aef68
Nicholas Hinzman <nicholashinzman@Nicholass-MacBook-Pro.local>
2019-10-06 21:43:26 -0500

    After tutorial completion.

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,84 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+  getPosts();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+$('.search').click(function(){
+  $('.city').text('');
+  $('.temp').text('');
+  $('.error-message').text('');
+  getWeather();
+})
+
+function getWeather(searchQuery){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=Imperial&APPID="+apiKey;
+  $.ajax(url,{success:function(data){
+      $('.city').text(data.name);
+      $('.temp').text(data.main.temp);
+    }, error:function(error){
+      $('.error-message').text('Oh Gosh, An Error Has occurred.');
+  }})
+}
+
+function searchWeather(){
+  var searchQuery = $('.search').val();
+  getWeather(searchQuery);
+}
+
+
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+}
+
+function addMessage(postTitle,postBody){
+  var postData = {
+    title: postTitle,
+    body: postBody
+  }
+
+  var database = firebase.database().ref("posts");
+  // Create a new post reference with an auto-generated id
+  var newPostRef = database.push();
+  newPostRef.set(postData, function(error) {
+    if (error) {
+      alert("ERROR!");
+    } else {
+      window.location.reload();
+    }
+  });
+}
+
+function handleMessageFormSubmit(){
+  var postTitle = $('#post-title').val();
+  var postBody = $('#post-body').val();
+  addMessage(postTitle,postBody);
+}
+
+function getPosts(){
+  return firebase.database().ref('posts').once('value').then(function(snapshot) {
+    var posts = snapshot.val();
+    console.log(posts);
+
+    for(var postKey in posts){
+      var post = posts[postKey];
+      $('#post-listing').append('<div>'+post.title+' - '+post.body+'</div>');
+    }
+  });
 }
